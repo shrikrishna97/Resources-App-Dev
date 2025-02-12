19f401c263afad4b5ec8d263af807cd2575866ec
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2025-02-12 20:15:10 +0530

    Update app.py
    First session code.

--- App-Dev-1/week-5/App/app.py
+++ App-Dev-1/week-5/App/app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template
 
+appp = Flask(__name__)
+
+@appp.route('/') #base url 
+def hello_world():
+    return 'Hello World! hi wo'
+
+@appp.route('/hello') #
+def hello():
+    users =[
+        {'username': "hi",'password': "123"}, #row 1
+            {'username': "world"}, 
+            {'username': "hello"}
+            ]
+    return render_template('happy.html', users=users )
+
+if __name__ == '__main__':
+    appp.run(debug=True)
