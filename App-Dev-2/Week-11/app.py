d4ac1b8f1046f60fb0023281e946765afd34a5d9
Shrikrishna <shrikrishnapandey72@gmail.com>
2025-08-12 22:19:33 +0530

    updated file after the session

--- App-Dev-2/Week-11/app.py
+++ App-Dev-2/Week-11/app.py
@@ -7,27 +7,29 @@ app = Flask(__name__)
 # Config from Flask documentation style
 app.config.from_mapping({
     "CACHE_TYPE": "RedisCache",
-    "CACHE_REDIS_HOST": "localhost",
-    "CACHE_REDIS_PORT": 6379,
-    "CACHE_REDIS_DB": 0,
-    "CACHE_REDIS_URL": "redis://localhost:6379/0",
+    # "CACHE_REDIS_HOST": "localhost",
+    # "CACHE_REDIS_PORT": 6379,
+    # "CACHE_REDIS_DB": 0,
+    "CACHE_REDIS_URL": "redis://localhost:6379/1",
     "CACHE_DEFAULT_TIMEOUT": 60,
-    "CACHE_KEY_PREFIX": "flask_cache_",
+    # "CACHE_KEY_PREFIX": "",
 })
 
+# flask_cache_ + view/ + url(/)
 cache = Cache(app)
 
 
+
 # Cached full route with key_prefix
-@app.route("/")
-@cache.cached(timeout=30, key_prefix="homepage")
+@app.route("/home")
+@cache.cached(timeout=90) # you can use this also @cache.cached(timeout=30, key_prefix="home")
 def home():
     time.sleep(5)
     return f"Home generated at {time.time()}"
 
 # Memoized function result (different for each number)
 @app.route("/square/<int:num>")
-@cache.memoize(timeout=60)
+@cache.memoize(timeout=90)
 def square(num):
     time.sleep(2)  # Simulating heavy work
     return f"Square of {num} is {num * num}"
