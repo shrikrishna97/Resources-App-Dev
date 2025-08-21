2108d96c7bfa408d51d62cdc85192a6718293515
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2025-08-21 15:48:34 +0530

    cors.py file added

---
+++ App-Dev-2/cors.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# Enable CORS with credentials support
+CORS(app, supports_credentials=True,
+     origins=["http://127.0.0.1:5500", "http://localhost:5500"])
+
+@app.route("/set-cookie")
+def set_cookie():
+    resp = make_response(jsonify({"message": "Cookie is set!"}))
+    resp.set_cookie(
+        "user_token",
+        "abc123",
+        # max_age=10 # seconds
+        # expires=datetime.utcnow() + timedelta(minutes=1) # deletes in 1 minute
+        httponly=True,    # JS cannot read this cookie
+        # secure=False,     # True in production with HTTPS
+        secure=True,
+        samesite=None,
+        # samesite="Lax"    # Works locally for cross-site cookies
+    )
+    return resp
+
+@app.route("/get-cookie")
+def get_cookie():
+    token = request.cookies.get("user_token")
+    if token:
+        return jsonify({"message": "Cookie retrieved!", "token": token})
+    return jsonify({"message": "No cookie found!"}), 404
+
+@app.route("/delete-cookie")
+def delete_cookie():
+    resp = make_response({"msg": "Cookie deleted"})
+    resp.delete_cookie("auth_token")
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
