9733a18e2ff9b62f014656e69ae405d9f222f916
Shrikrishna <shrikrishnapandey72@gmail.com>
2026-06-26 20:22:51 +0530

    new file added for day-1 of the session

---
+++ App-Dev-2-Project/May 26/Day-1/backend/model.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default='user')
+
+# class ParkingLot(db.Model):
+
+# class ParkingSpot(db.Model):
+
+# class Rservation(db.Model):
