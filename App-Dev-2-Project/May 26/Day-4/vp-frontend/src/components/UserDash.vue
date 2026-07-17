963a78521deaea58435e9fdf50d8df83d6de7775
shrikrishnapandey72@gmail.com <your_email@example.com>
2026-07-17 21:05:27 +0530

    Adding Day-4 content

---
+++ App-Dev-2-Project/May 26/Day-4/vp-frontend/src/components/UserDash.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    User Dashboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserDash'
+}
+</script>
---
+++ App-Dev-2-Project/May 26/Day-4/vp-frontend/src/views/UserView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    User
+    <div v-if="!isloggedIn">
+      <div v-if="!isRegistered"><UserLogin @loggedIn-success="handleLoginSuccess" @register-success="isRegistered = true"/></div>
+      <div v-else><UserRegister @registration-success="handleRegistration" @login-success="isRegistered = false" /></div>
+    </div>
+    <div v-else>
+      <UserDash />
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDash from '@/components/UserDash.vue'
+import UserRegister from '@/components/UserRegister.vue'
+import UserLogin from '@/components/UserLogin.vue'
+
+export default {
+  name: 'UserView',
+  components: {
+    UserDash,
+    UserRegister,
+    UserLogin,
+  },
+  data() {
+    return {
+      isloggedIn: false,
+      isRegistered: false,
+    }
+  },
+  methods: {
+    handleLoginSuccess() {
+      this.isloggedIn = true
+    },
+    handleRegistration() {
+      this.isRegistered = false
+      this.isloggedIn = false
+      console.log(this.isRegistered)
+      alert('Registration successful! You can now log in.')
+    },
+  },
+}
+</script>
